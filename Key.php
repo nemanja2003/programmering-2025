ec8995dfe5ee5cc9946d24b08fd7c6aa9c531a86
nemanja2003 <nemanjasakroketone@gmail.com>
2025-10-27 12:43:13 +0100

    Create Key.php

---
+++ Key.php
@@ -0,0 +1,13 @@
+<?php  /* db-tilkobling */
+/*
+/*  Programmet foretar tilkobling til database-server og valg av database
+*/
+
+  $servername="localhost";
+  $username="265635";
+  $password="2lIlrHPV";
+  $database="265635"; /* verdier satt for vert, bruker, passord og database for tilkobling til databaseserver */
+
+
+$db = mysqli_connect($servername,$username,$password,$database) or die ("Could not connect to server...");
+?>
